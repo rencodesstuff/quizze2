1990492f6d2c2e91d67b6e2badc10ff563a8d9a1
rendnsh <realaimxn@gmail.com>
2024-11-08 19:51:04 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,18 +17,6 @@ const nextConfig = {
             key: 'Feature-Policy', 
             value: 'camera *' 
           },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp'
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin'
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'cross-origin'
-          }
         ],
       },
     ];
